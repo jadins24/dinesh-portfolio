c11c56f1e9610bd63acbb3a15cb98f46044cc109
jadins24 <jadinesh04@gmail.com>
2023-02-04 09:43:23 +0530

    add

--- js/main.js
+++ js/main.js
@@ -12,4 +12,14 @@ $('.i-slide').owlCarousel({
 		
 	}
 });
+function downloadFile(filePath) {
+    var link = document.createElement("a");
+    link.href = filePath;
+    link.download = filePath.split("/").pop();
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
+
 
